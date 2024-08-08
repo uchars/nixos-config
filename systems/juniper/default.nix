bdf782cf21cbe9dd8ca614777f541ab0a7b7cd0c
Nils <40796807+uchars@users.noreply.github.com>
2024-08-08 17:43:56 +0000

    update

--- systems/juniper/default.nix
+++ systems/juniper/default.nix
@@ -93,7 +93,6 @@
   time.timeZone = "Europe/Berlin";
   networking.hostName = "juniper";
 
-  virtualisation.docker.storageDriver = "overlay2";
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
