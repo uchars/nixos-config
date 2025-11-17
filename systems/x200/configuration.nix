cce9cdf2f32218990ab1b5da8835c7f75141740b
Nils <40796807+uchars@users.noreply.github.com>
2025-11-17 18:56:31 +0000

    .

--- systems/x200/configuration.nix
+++ systems/x200/configuration.nix
@@ -12,13 +12,14 @@
 
   boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
 
+  boot.loader.efi.canTouchEfiVariables = true;
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
-  # boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiSupport = true;
   # boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
-  # boot.loader.grub.device = "nodev"; # or "nodev" for efi only
+  boot.loader.grub.device = "nodev"; # or "nodev" for efi only
 
 
   settings.networking = {
